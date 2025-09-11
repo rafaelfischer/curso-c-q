96bf9416370749af3f6e6992c9cb43e569a597da
Rodrigo Fischer <rodrigobfischer@gmail.com>
2025-09-11 17:54:22 -0300

    1st version

---
+++ exercicios/ex_base.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    //tutorial: https://www.tutorialspoint.com/cprogramming/index.htm
+    //C online compiler: https://www.onlinegdb.com/online_c_compiler
+
+
+    //declaring a variable
+    //int number;
+    //number = 10; //assignment
+    //int number = 10; //declaration + assignment
+    //reading a variable from keyboard
+    //scanf("%d", &number);
+    //printing a variable
+    //printf("The number is %d\n", number);
+    return 0;
+}
