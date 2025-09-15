e4bf957b4ed8315b739547b1c134eec77878835f
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 14:41:39 -0300

    Alteração do código e compilação do exercício 2

---
+++ exercicios/exercicio2/exercicio2.c
@@ -0,0 +1,17 @@
+/*
+    Objetivo do programa: escrever na tela Ola Mundo
+    Data da criacao: 2025-08-28
+    Criado por: @programacaomentoria
+    Alterado por: @rafaelfischer
+*/
+#include <stdio.h>
+int main() {
+	char nome[200];
+	int idade;
+	
+	printf("Digite seu nome: ");
+	scanf("%s", nome);
+	printf("Digite sua idade: ");
+	scanf("%d", &idade);
+	printf("Ola, %s! Voce tem %d anos.\n", nome, idade);
+}
