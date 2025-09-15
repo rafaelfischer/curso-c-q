c30b9016e899baec531460eb3fb60cee46bc0077
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-15 14:37:57 -0300

    Organização das pastas, alteração do código e compilação do exercício 1

---
+++ exercicios/exercicio1/exercicios1.c
@@ -0,0 +1,11 @@
+/*
+    Objetivo do programa: escrever na tela Ola Mundo
+    Data da criacao: 2025-08-28
+    Criado por: @programacaomentoria
+    Alterado por: @rafaelfischer
+*/
+#include <stdio.h>
+int main() {
+    printf("Hello World!");
+    return 0;
+}
